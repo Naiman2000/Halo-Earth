d1a8c9f201465a8d7368a68c8b6b2ca35891bc75
werontan20 <werontan2017@gmail.com>
2025-12-14 10:35:02 +0800

    About page and index page background updated

--- src/app/pages/public/landing/landing.ts
+++ src/app/pages/public/landing/landing.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SiteSettingsService } from '../../../services/site-settings.service';
 
 @Component({
   selector: 'app-landing',
+  standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
 export class Landing {
+  private siteSettingsService = inject(SiteSettingsService);
+  settings$ = this.siteSettingsService.getSettings();
 }
 
